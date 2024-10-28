9faff1bf93ef9b9d312537323b09a1f8d2f3db65
davidnoya <dnoyav23@fpcoruna.afundacion.org>
2024-10-28 10:19:18 +0100

    Ejercicio 4-5-6, sprint 2 PHP y SQL

--- sprint2phpsql/www/mysite/detail.php
+++ sprint2phpsql/www/mysite/detail.php
@@ -12,4 +12,22 @@ echo "<h1>".$book['nombre']."</h1>";
 echo "<h2>Temática: ".$book['tematica']."</h2>";
 echo "<h2>Fecha de publicación: ".$book['fecha_publicacion']."</h2>";
 echo "<img src='".$book['url_imagen']."' alt='Portada'>";
+
+echo "<h3>Comentarios:</h3><ul>";
+$query_comments = "SELECT * FROM tComentarios WHERE juego_id = $id";
+$result_comments = mysqli_query($db, $query_comments) or die('Error en la consulta de comentarios');
+while ($comment = mysqli_fetch_array($result_comments)) {
+    echo "<li>".$comment['comentario']." -<small>".$comment['fecha']."</small></li>";
+}
+echo "</ul>";
+?>
+
+<p>Deja un nuevo comentario:</p>
+<form action="/comment.php" method="post">
+<textarea rows="4" cols="50" name="new_comment"></textarea><br>
+<input type="hidden" name="juego_id" value="<?php echo $juego_id; ?>">
+<input type="submit" value="Comentar">
+</form>
+<?php
+mysqli_close($db);
 ?>
