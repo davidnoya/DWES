5e4e0f382a681918532e8e3294620a7640ddcb80
davidnoya <dnoyav23@fpcoruna.afundacion.org>
2025-01-19 18:02:20 +0100

    UsuarioPersonalizado

--- practicaSegundoTrimestre/practica/eventos/models.py
+++ practicaSegundoTrimestre/practica/eventos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class usuarioPersonalizado(AbstractUser):
+    TIPO_USUARIO = [
+        ('organizador', 'Organizador'),
+        ('participante', 'Participante'),
+    ]
+    tipo = models.CharField(max_length=20, choices=TIPO_USUARIO, default='participante')
