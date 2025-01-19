a817956baba70d50e1beb08bf2204709b5a95ddf
davidnoya <dnoyav23@fpcoruna.afundacion.org>
2025-01-19 18:11:28 +0100

    Modelo vevento

--- practicaSegundoTrimestre/practica/eventos/models.py
+++ practicaSegundoTrimestre/practica/eventos/models.py
@@ -7,3 +7,14 @@ class usuarioPersonalizado(AbstractUser):
         ('participante', 'Participante'),
     ]
     tipo = models.CharField(max_length=20, choices=TIPO_USUARIO, default='participante')
+
+class Evento(models.Model):
+    organizador = models.ForeignKey(usuarioPersonalizado, on_delete=models.CASCADE, related_name='eventos')
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    fecha = models.DateTimeField()
+    capacidad = models.PositiveIntegerField()
+    url_imagen = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.titulo
