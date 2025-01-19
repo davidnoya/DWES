9dfa7b31462e818386e7cf9a33beb7ca9d9fe33a
davidnoya <dnoyav23@fpcoruna.afundacion.org>
2025-01-19 18:44:55 +0100

    cConfiguración del administrador de Django

--- practicaSegundoTrimestre/practica/eventos/admin.py
+++ practicaSegundoTrimestre/practica/eventos/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import usuarioPersonalizado, Evento, Reserva, Comentario
 
-# Register your models here.
+@admin.register(usuarioPersonalizado)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'rol')
+    list_filter = ('rol',)
+
+@admin.register(Evento)
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'fecha', 'capacidad', 'organizador')
+    search_fields = ('titulo', 'descripcion')
+    list_filter = ('fecha', 'organizador')
+
+@admin.register(Reserva)
+class ReservaAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'evento', 'num_tickets', 'estado')
+    list_filter = ('estado',)
+
+@admin.register(Comentario)
+class ComentarioAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'evento', 'fecha')
+    search_fields = ('comentario',)
--- practicaSegundoTrimestre/practica/eventos/models.py
+++ practicaSegundoTrimestre/practica/eventos/models.py
@@ -8,6 +8,9 @@ class usuarioPersonalizado(AbstractUser):
     ]
     tipo = models.CharField(max_length=20, choices=TIPO_USUARIO, default='participante')
 
+    def __str__(self):
+        return self.username
+
 class Evento(models.Model):
     organizador = models.ForeignKey(usuarioPersonalizado, on_delete=models.CASCADE, related_name='eventos')
     titulo = models.CharField(max_length=100)
@@ -25,13 +28,13 @@ class Reserva(models.Model):
         ('confirmada', 'Confirmada'),
         ('cancelada', 'Cancelada'),
     ]
-    participante = models.ForeignKey(usuarioPersonalizado, on_delete=models.CASCADE, related_name='reservas')
+    usuario= models.ForeignKey(usuarioPersonalizado, on_delete=models.CASCADE, related_name='reservas')
     evento = models.ForeignKey(Evento, on_delete=models.CASCADE, related_name='reservas')
     num_tickets = models.PositiveIntegerField()
     estado = models.CharField(max_length=20, choices=ESTADOS, default='pendiente')
 
     def __str__(self):
-        return f'Reserva de {self.participante.username} para {self.evento.titulo}'
+        return f'Reserva de {self.usuario.username} para {self.evento.titulo}'
 
 class Comentario(models.Model):
     usuario = models.ForeignKey(usuarioPersonalizado, on_delete=models.CASCADE, related_name='comentarios')
